714d453e6e1c5e18a9e0bbd747dc7096e7ab1a48
jamorbal <jesus.amor.next@bbva.com>
2020-11-25 13:00:46 +0100

    Version inicial del chatBot

---
+++ src/test/java/com/bbva/tinfoilhat/NativeTelegramChatBotIT.java
@@ -0,0 +1,9 @@
+package com.bbva.tinfoilhat;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeTelegramChatBotIT extends TelegramChatBotTest {
+
+    // Execute the same tests but in native mode.
+}
